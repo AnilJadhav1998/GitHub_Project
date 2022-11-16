efcb7d26d171de6d0c46f809620e76067be871a7
AnilJadhav1998 <anilvjadhav099@gmail.com>
2022-11-16 14:33:26 +0530

    Forth commit

---
+++ GitHub_Project/src/com/java/gihub/project/example/Demo4.java
@@ -0,0 +1,20 @@
+package com.java.gihub.project.example;
+
+import java.util.ArrayList;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		
+		ArrayList al= new ArrayList();
+		al.add("Gopal");
+		al.add("Anil");
+		al.add("Shiva");
+		al.add("Prasad");
+		
+		System.out.println(al);
+		
+
+	}
+
+}
