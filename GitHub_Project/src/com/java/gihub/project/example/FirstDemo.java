fa335626cc44354463c1d8245674914c595ffcc1
AnilJadhav1998 <anilvjadhav099@gmail.com>
2022-11-16 12:37:15 +0530

    First Commit

---
+++ GitHub_Project/src/com/java/gihub/project/example/FirstDemo.java
@@ -0,0 +1,21 @@
+package com.java.gihub.project.example;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		
+		HashMap hashmap= new HashMap();
+		hashmap.put("Swapnil", 75);
+		hashmap.put("Raj", 80);
+		hashmap.put("Gopal", 92);
+		hashmap.put("Anil", 84);
+		
+		System.out.println(hashmap);
+		
+
+	}
+
+}
